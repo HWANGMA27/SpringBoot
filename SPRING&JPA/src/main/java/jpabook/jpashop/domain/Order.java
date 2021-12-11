e43a979424e14f5efaa9ff7212adeaff8fd4cf13
hwangma27 <pikacu22@kakao.com>
2021-12-11 16:40:02 +0900

    [ADD] Member, Delivery, Orders, Order_item, Item, Category Entity 생성

---
+++ SPRING&JPA/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItemList = new ArrayList<>();
+
+    // order를 통해 delivery를 접근할 가능성이 높다는 가정하에 연관관계의 주인을 order의 delivery로 설정
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
