117bd41160264061484e2fe42624ff0f49a59324
hwangma27 <pikacu22@kakao.com>
2021-12-15 16:30:25 +0900

    [ADD] 주문 검색 기능 추가

--- SPRING&JPA/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ SPRING&JPA/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,9 +2,15 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import javax.script.ScriptEngine;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -21,7 +27,35 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(){
-//
-//    }
+    //using criteria
+    public List<Order> findAllByCriteria(OrderSearch orderSearch){
+//        return em.createQuery("select o from Order o join o.member m"
+//                        + " where o.status = :status"
+//                        + " and m.name like :name", Order.class)
+//                .setParameter("status", orderSearch.getOrderStatus())
+//                .setParameter("name", orderSearch.getMemberName())
+//                .setMaxResults(1000)
+//                .getResultList();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        //주문 상태 검색
+        if(orderSearch.getOrderStatus() != null){
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+
+        //회원이름 검색
+        if(StringUtils.hasText(orderSearch.getMemberName())){
+            Predicate name = cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+    }
 }
