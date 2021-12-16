cf19b54f8eee8503edfe762a7686191f09bff277
hwangma27 <pikacu22@kakao.com>
2021-12-16 17:04:36 +0900

    [ADD] 회원 조회 화면 & Controller 추가

--- SPRING&JPA/src/main/java/jpabook/jpashop/service/MemberService.java
+++ SPRING&JPA/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -3,7 +3,6 @@ package jpabook.jpashop.service;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,7 +33,7 @@ public class MemberService {
     }
 
     //회원 전체 조회
-    public List<Member> findMember(){
+    public List<Member> findMembers(){
         return memberRepository.findAll();
     }
 
